9e88b5a3903db9764ecc4ab8ba4d32f972378eeb
kienlhpc05751 <139949358+kienlhpc05751@users.noreply.github.com>
2024-05-25 23:32:45 +0700

    demo form sale (kien)

--- code/e-project/src/Pages/Shop.js
+++ code/e-project/src/Pages/Shop.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Sale  from "../Components/Hero/sale";
 const Shop=()=>{
     return(
         <div>
           <Hero/>  
+          <Sale></Sale>
+          <Sale></Sale>
+          <Sale></Sale>
+          <Sale></Sale>
+          <Sale></Sale>
+          <Sale></Sale>
+
         </div>
     )
 }
